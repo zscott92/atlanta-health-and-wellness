bcc48ca5aa52ac31eb87f311b80faeb5baa13853
Zach Scott <zscott92@gmail.com>
2020-10-22 20:50:02 -0400

    Initial Commit

---
+++ Atlanta Center For Wellness/Atlanta_Center_For_WellnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  Atlanta_Center_For_WellnessApp.swift
+//  Atlanta Center For Wellness
+//
+//  Created by Zach Scott on 10/22/20.
+//
+
+import SwiftUI
+
+@main
+struct Atlanta_Center_For_WellnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
